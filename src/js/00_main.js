b3acf3bb17fc2dc83e9679a975aa7d69304ae6d9
modehans <monicaglezsanch@gmail.com>
2022-07-16 18:10:52 +0200

    detect when the image is generic and replace it with an image with the name of the series

--- src/js/00_main.js
+++ src/js/00_main.js
@@ -6,16 +6,26 @@ let seriesSearch = [];
 
 const renderCards = (arrayData) => {
   let html = '';
+  const imageNotFound =
+    'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png';
+
   for (const oneSerie of arrayData) {
+    let urlImageSerie = `${oneSerie.images.jpg.image_url}`;
+    const newImage = `https://via.placeholder.com/210x295/ffffff/666666/?text=${oneSerie.title}`;
     html += ` <div class="card js-card">`;
     html += ` <h3 class="card__title">${oneSerie.title}</h3>`;
-    html += ` <img class="card__image" src="${oneSerie.images.jpg.image_url}"/>`;
+    if (urlImageSerie === imageNotFound) {
+      html += ` <img class="card__image" src="${newImage}"/>`;
+    } else {
+      html += ` <img class="card__image" src="${urlImageSerie}"/>`;
+    }
     html += `</div>`;
   }
   resultsSearch.innerHTML = html;
 };
 
-const handleClickSearch = () => {
+const handleClickSearch = (ev) => {
+  ev.preventDefault();
   const nameUserSerie = userInput.value.toLowerCase();
   fetch(`https://api.jikan.moe/v4/anime?q=${nameUserSerie}`)
     .then((response) => response.json())
