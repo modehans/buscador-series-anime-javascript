0db5648d810a69274066042aaa58e440999a569e
modehans <monicaglezsanch@gmail.com>
2022-07-17 14:58:09 +0200

    Update favorites on load

--- src/js/04-load.js
+++ src/js/04-load.js
@@ -1,9 +1,7 @@
 function onLoad() {
-  const dataLS = JSON.parse(localStorage.getItem('data'));
-  if (dataLS !== '') {
-    favoriteList.innerHTML = renderCards(dataLS);
-  } else {
-    favoriteList.innerHTML = '';
+  favorites = JSON.parse(localStorage.getItem('data'));
+  if (favorites !== '') {
+    renderFavSeries();
   }
 }
 
