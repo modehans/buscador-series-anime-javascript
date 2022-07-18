5bb47ac00b062b2e9d204e5a0f4981cac1051c71
modehans <monicaglezsanch@gmail.com>
2022-07-18 08:56:16 +0200

    Delete debugger in Load file.js

--- src/js/04_load.js
+++ src/js/04_load.js
@@ -2,7 +2,6 @@
 function onLoad() {
   favorites = JSON.parse(localStorage.getItem('data'));
   if (favorites.length !== 0) {
-    debugger;
     renderFavSeries();
     console.log(favorites);
   }
